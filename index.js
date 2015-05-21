3792d9cab08b0a3c20272bb28c95d0c04008f6f6
bingo777 <bingo777mail@gmail.com>
2015-05-22 01:25:18 +0300

    include sockets.io

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendfile('index.html');
 });
 
-http.listen(3000, function () {
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function(){
   console.log('listening on *:3000');
 });
