aca4fb9dee79ec801614caea7eaf63ddacae8a26
bingo777 <bingo777mail@gmail.com>
2015-06-08 00:14:25 +0300

    first commit for game app

--- app.js
+++ app.js
@@ -1,9 +1,90 @@
 var socket = io();
-$('button').click(function(){
-  socket.emit('chat message', $('#m').val());
-  $('#m').val('');
-  return false;
+
+var arrowUpIsDown = false;
+var arrowDownIsDown = false;
+var arrowLeftIsDown = false;
+var arrowRightIsDown = false;
+
+socket.on('update', function (data) {
+  drawScreen(data);
 });
-socket.on('chat message', function(msg){
-  $('#messages').append($('<li>').text(msg));
+
+socket.on('initialize', function (data) {
+  drawScreen(data);
+  startGame();
 });
+
+function startGame() {
+  document.addEventListener('keydown', onKeyDown, false);
+  document.addEventListener('keyup', onKeyUp, false);
+}
+
+function endGame() {
+  document.removeEventListener('keydown', onKeyDown, false);
+  document.removeEventListener('keyup', onKeyUp, false);
+}
+
+function onKeyDown(e) {
+  switch (e.keyCode) {
+    case 38:
+      if (!arrowUpIsDown) {
+        socket.emit('playerMove', 'up');
+        arrowUpIsDown = true;
+      }
+      break;
+    case 40:
+      if (!arrowDownIsDown) {
+        socket.emit('playerMove', 'down');
+        arrowDownIsDown = true;
+      }
+      break;
+    case 37:
+      if (!arrowLeftIsDown) {
+        socket.emit('playerMove', 'left');
+        arrowLeftIsDown = true;
+      }
+      break;
+    case 39:
+      if (!arrowRightIsDown) {
+        socket.emit('playerMove', 'right');
+        arrowRightIsDown = true;
+      }
+      break;
+  }
+}
+
+function onKeyUp(e) {
+  switch (e.keyCode) {
+    case 38:
+      arrowUpIsDown = false;
+      break;
+    case 40:
+      arrowDownIsDown = false;
+      break;
+    case 37:
+      arrowLeftIsDown = false;
+      break;
+    case 39:
+      arrowRightIsDown = false;
+      break;
+  }
+  if (!arrowUpIsDown && !arrowDownIsDown && !arrowLeftIsDown && !arrowRightIsDown) {
+    socket.emit('playerMove', 'none');
+  }
+}
+
+function drawScreen(players) {
+  document.body.innerHTML = '';
+
+  for (var player in players) {
+    if (players.hasOwnProperty(player)) {
+      player = players[player];
+      var playerDiv = document.createElement('div');
+      playerDiv.style.backgroundColor = player.color;
+      playerDiv.style.left = player.x + 'px';
+      playerDiv.style.top = player.y + 'px';
+      playerDiv.className = 'player';
+      document.body.appendChild(playerDiv);
+    }
+  }
+}
--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var players = {};
+var colors = ['red', 'green', 'blue', 'black', 'orange'];
+var updateInterval = 50;
+var speed = 10;
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendfile('index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+app.get('/app.js', function (req, res) {
+  res.sendfile('app.js');
+});
+
+io.on('connection', function (socket) {
+  var playerColor = colors.pop();
+  players[playerColor] = {
+    x: 0,
+    y: 0,
+    color: playerColor,
+    direction: 'none'
+  };
+  socket.color = playerColor;
+  socket.emit('initialize', players);
+  socket.on('playerMove', function (dirrection) {
+    updatePosition(socket, dirrection);
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function () {
+  startGame();
 });
+
+function startGame() {
+  setInterval(function () {
+    for (var player in players) {
+      if (players.hasOwnProperty(player)) {
+        player = players[player];
+        switch (player.direction) {
+          case 'up':
+            player.y -= speed;
+            break;
+          case 'down':
+            player.y += speed;
+            break;
+          case 'left':
+            player.x -= speed;
+            break;
+          case 'right':
+            player.x += speed;
+            break;
+        }
+      }
+    }
+    io.emit('update', players);
+  }, updateInterval);
+}
+
+function updatePosition(socket, direction) {
+  var player = players[socket.color];
+  player.direction = direction;
+}
