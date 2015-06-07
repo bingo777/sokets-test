52713851a9c37ba7013c28499330614656f7cd6b
bingo777 <bingo777mail@gmail.com>
2015-06-07 21:07:44 +0300

    move js logic out of html

---
+++ app.js
@@ -0,0 +1,9 @@
+var socket = io();
+$('button').click(function(){
+  socket.emit('chat message', $('#m').val());
+  $('#m').val('');
+  return false;
+});
+socket.on('chat message', function(msg){
+  $('#messages').append($('<li>').text(msg));
+});
